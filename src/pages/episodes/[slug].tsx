2650b29dc2037c0d87b98bd2364cce761904e852
Itzac Angelo Albertin <itzacalbertin@gmail.com>
2021-04-22 12:25:42 -0300

    added play, pause and some stuff

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -8,6 +8,7 @@ import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss'
+import { route } from 'next/dist/next-server/server/router';
 
 type Episode = {
     id: string;
@@ -54,6 +55,22 @@ export default function Episode({episode}: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const {data} = await api.get('episodes', {
+        params: {
+            _limit:2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
     return{
         paths: [],
         fallback: 'blocking'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import { api } from "../services/api"
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString"
 
 import styles from './home.module.scss'
+import { useContext } from "react"
+import { PlayerContext } from "../contexts/PlayerContext"
 
 type Episode = {
   id: string;
@@ -26,6 +28,8 @@ type HomeProps = {
 }
 
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  const {play} = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -46,7 +50,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button>
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/images/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
@@ -84,7 +88,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <td style= {{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button">
+                    <button type="button" onClick={() => play(episode)}>
                       <img src="/images/play-green.svg" alt="Tocar episódio"/>
                     </button>
                   </td>
